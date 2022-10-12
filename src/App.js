a77951f8d39c86a5c5e07121c106ec8085c541be
unknown <ivancampos039@gmail.com>
2022-10-12 19:53:05 -0300

    Aulas de estilizacao em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+  const n = 14
+  const [name] = useState("Ivan")
+  const redTitle = true
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Css Global*/}
+      <h1>React com CSS</h1>
+      {/*Css Component*/}
+      <MyComponent/>
+      <p>Este e um paragrafo do app.js</p>
+      {/*Css inline */}
+      <p style= {{
+        color: "blue",
+        padding: "25px",
+        borderTop:"2px solid red",
+      }}>Este e um paragrafo inline</p>
+
+      {/*Css inline  dinâmico*/}
+      <h2 style={n < 10 ? ({color: "green"}) : ({color:"purple"})}>Css Dinâmico</h2>
+      <h2 style={n < 15 ? ({color: "green"}) : ({color:"purple"})}>Css Dinâmico</h2>
+      <h2 style={name === "Ivan" ? ({color: "green", background:"#000"}) : null }>Css Dinâmico</h2>
+
+      {/*Css classe  dinâmico*/}
+
+      <h2 className={redTitle ? "red-title" : "title"}>Este título é dinâmico</h2>
+
+      {/*Css Modules */}
+      <Title/>
+      <h2 className='my-title'> testando</h2>
+
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css"
+const MyComponent = () => {
+  return (
+    <div>
+        <h1> Css de Component</h1>
+        <p>Este é o parágrafo do component</p>
+        <p className="my-comp-paragraph">Esse tambem e um parágrafo do Component</p>
+    </div>
+  )
+}
+
+export default MyComponent
